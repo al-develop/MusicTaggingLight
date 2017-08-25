bdc98f26350dd1c074877542e039314a1d5c3102
Al <al.pankratz@hotmail.de>
2017-08-25 10:19:41 +0200

    UI Design; added some more Icons; edited About Area

--- MusicTaggingLight/MainWindow.xaml.cs
+++ MusicTaggingLight/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +13,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Ookii.Dialogs.Wpf;
 
 namespace MusicTaggingLight
 {
@@ -20,9 +22,24 @@ namespace MusicTaggingLight
     /// </summary>
     public partial class MainWindow : Window
     {
+        private MainWindowViewModel vm;
         public MainWindow()
         {
             InitializeComponent();
+            this.DataContext = vm = new MainWindowViewModel();
+            vm.SelectRootFolderFunc = new Func<string>(SelectRootFolderDialog);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns>Returns the root directory of the selected folder</returns>
+        private string SelectRootFolderDialog()
+        {
+            var dialog = new VistaFolderBrowserDialog();
+            if (dialog.ShowDialog() == true)
+                return dialog.SelectedPath;
+            return "";
         }
     }
 }
---
+++ MusicTaggingLight/MainWindowViewModel.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using DevExpress.Mvvm;
+using File = TagLib.File;
+
+namespace MusicTaggingLight
+{
+    public class MainWindowViewModel : ViewModelBase
+    {
+        public Func<string> SelectRootFolderFunc { get; set; }
+        public Action ExitAction { get; set; }
+
+        public ICommand SelectRootFolderCommand { get; set; }
+        public ICommand SaveCommand { get; set; }
+        public ICommand SearchOnlineCommand { get; set; }
+        public ICommand ExitCommand { get; set; }
+
+
+        private ObservableCollection<object> _musicFiles;
+        private object _selectedFile;
+
+        public object SelectedFile
+        {
+            get { return _selectedFile; }
+            set { SetProperty(ref _selectedFile, value, () => SelectedFile); }
+        }
+        public ObservableCollection<object> MusicFiles
+        {
+            get { return _musicFiles; }
+            set { SetProperty(ref _musicFiles, value, () => MusicFiles); }
+        }
+
+
+        public MainWindowViewModel()
+        {
+            MusicFiles = new ObservableCollection<object>();
+            InitCommands();
+
+        }
+
+        private void InitCommands()
+        {
+            SelectRootFolderCommand = new DelegateCommand(SelectRootFolder);
+            SaveCommand = new AsyncCommand(Save);
+            SearchOnlineCommand = new AsyncCommand(SearchOnline);
+            ExitCommand = new DelegateCommand(() => ExitAction.Invoke());
+
+        }
+
+        private void SelectRootFolder()
+        {
+            var root = SelectRootFolderFunc.Invoke();
+            if (String.IsNullOrEmpty(root))
+                return;
+
+            LoadMusicFilesFromRoot(root);
+        }
+
+        private Task SearchOnline()
+        {
+            throw new NotImplementedException();
+        }
+
+        private Task Save()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void LoadMusicFilesFromRoot(string root)
+        {
+            var subfolders = GetSubfolders(root);
+            foreach (var folder in subfolders)
+            {
+                var files = Directory.GetFiles(folder, "*.mp3");
+                foreach (var file in files)
+                {
+                    var tagInfo = TagLib.Mpeg.File.Create(file);
+                }
+            }
+        }
+
+        private IEnumerable<string> GetSubfolders(string sourcePath)
+        {
+            if (!Directory.Exists(sourcePath))
+                return new List<string>();
+
+            IEnumerable<string> subfolders = Directory.GetDirectories(sourcePath, "*", SearchOption.AllDirectories)
+                                                      .Where(f => !Directory.EnumerateDirectories(f).Any());
+            return subfolders;
+        }
+    }
+}
