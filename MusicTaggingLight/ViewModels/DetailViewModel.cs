6c91419f6a546c8f7f1f845cde7ac918225d557e
Jose Vega <josevega234@gmail.com>
2020-04-12 00:42:55 +0200

    HouseKeeping :)

--- MusicTaggingLight/ViewModels/DetailViewModel.cs
+++ MusicTaggingLight/ViewModels/DetailViewModel.cs
@@ -13,69 +13,19 @@ namespace MusicTaggingLight.ViewModels
     public class DetailViewModel : ViewModelBase
     {
 		private MainWindowViewModel mwvm;
-		private MusicFileTag mfTag;
+		private MusicFileTag musicFileTag;
 
-		#region View Properties
-
-		private byte[] _albumCover;
-		private uint _track;
-		private uint _year;
-		private string _album;
-		private string _genre;
-		private string _title;
-		private string _artist;
-		private string _fileName;
-		private string _comment;
+		public MusicFileTag MusicfileTag
+		{
+			get { return musicFileTag; }
+			set 
+			{
+				SetProperty(ref musicFileTag, value, () => MusicfileTag); 
+			}
+		}
 
 
-		public byte[] AlbumCover
-		{
-			get { return _albumCover; }
-			set { SetProperty(ref _albumCover, value, () => AlbumCover); }
-		}
-        public uint Track
-		{
-			get { return _track; }
-			set { SetProperty(ref _track, value, () => Track); }
-		}
-		public uint Year
-		{
-			get { return _year; }
-			set { SetProperty(ref _year, value, () => Year); }
-		}
-		public string Album
-		{
-			get { return _album; }
-			set { SetProperty(ref _album, value, () => Album); }
-		}
-		public string Genre
-		{
-			get { return _genre; }
-			set { SetProperty(ref _genre, value, () => Genre); }
-		}
-		public string Title
-		{
-			get { return _title; }
-			set { SetProperty(ref _title, value, () => Title); }
-		}
-		public string Artist
-		{
-			get { return _artist; }
-			set { SetProperty(ref _artist, value, () => Artist); }
-		}
-		public string FileName
-		{
-			get { return _fileName; }
-			set { SetProperty(ref _fileName, value, () => FileName); }
-		}
-		public string Comment
-		{
-			get { return _comment; }
-			set { SetProperty(ref _comment, value, () => Comment); }
-		}
-        #endregion
-
-        #region Commands
+		#region Commands
 
 		public ICommand SaveTagsCommand { get; set; }
 
@@ -95,34 +45,13 @@ namespace MusicTaggingLight.ViewModels
 
 		public void SetItemToShow(MusicFileTag fileTag)
 		{
-			mfTag = fileTag;
+			MusicfileTag = fileTag;
 
-			Title = mfTag.Title;
-			Artist = mfTag.Artist;
-			Album = mfTag.Album;
-			Track = mfTag.Track;
-			AlbumCover = mfTag.AlbumCover;
-			Year = mfTag.Year;
-			Genre = mfTag.Genre;
-			FileName = mfTag.FileName;
-			Comment = mfTag.Comment;
 		}
 	
 		public void Save()
 		{
-			
-
-			mfTag.Title = Title;
-			mfTag.Artist = Artist;
-			mfTag.Album = Album;
-			mfTag.Track = Track;
-			mfTag.AlbumCover = AlbumCover;
-			mfTag.Year = Year;
-			mfTag.Genre = Genre;
-			mfTag.FileName = FileName;
-			mfTag.Comment = Comment;
-
-			mwvm.SaveCommand.Execute(mfTag);
+			mwvm.SaveCommand.Execute(MusicfileTag);
 		}
 	}
 }
